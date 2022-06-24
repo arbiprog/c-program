19479849ac695a87f96a90758fd3fdf9d49232da
taha <taharca12@gmail.com>
2022-06-24 15:04:19 +0100

    first upload

---
+++ Untitled-1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main(){
+  printf("مرحبا أنا أبرمج");
+  return 0;
+};
+
+
+
+
+
+
+
+
+
+
+/*
+#include <stdio.h>
+int main(){
+  int pr=3;
+  printf("%d" , pr);
+  return 0;
+};
+*/
+
+
+/*
+#include <stdio.h>
+
+
+int main(){
+  int w1=30, w2=12;
+  printf("هذي هي حالة العجلات :\n");
+  printf("%d %d\n", w1,w2);
+  printf("%d %d" , w2,w1);
+  return 0;
+}
+*/
